bc38eb6a164a5d49b8865cdcd3ac91ea574748c6
Yttehs <shetty.yttehs@outlook.com>
2024-09-19 21:08:48 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -4,4 +4,18 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+		int len = 3;
+		int list[] = {a, b, c};
+		for (int i = 0; i < len; ++i) {
+				for (int j = 0; j < len - i - 1; ++j) {
+						if (list[j] > list[j + 1]) {
+								int tmp = list[j];
+								list[j] = list[j + 1];
+								list[j + 1] = tmp;
+						}
+				}
+		}
+		result.min = list[0];
+		result.mid = list[1];
+		result.max = list[2];
 }
